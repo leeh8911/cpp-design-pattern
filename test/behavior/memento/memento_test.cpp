e7234e8ef602c6a296ff5ea63f0da5af3f1233a8
sangwon <leeh8911@gmail.com>
2022-10-29 05:36:39 +0900

    update memento test

--- test/behavior/memento/memento_test.cpp
+++ test/behavior/memento/memento_test.cpp
@@ -16,10 +16,58 @@
 namespace
 {
 using namespace design_pattern::behavior::memento;
-TEST(MementoTest, Sample)
+
+TEST(MementoTest, MementoCreate)
+{
+    Memento m("state string");
+    EXPECT_EQ(m.State(), "state string");
+}
+
+TEST(MementoTest, OriginatorCreate)
+{
+    Originator o("state string");
+    EXPECT_EQ(o.State(), "state string");
+}
+
+TEST(MementoTest, OriginatorSave)
+{
+    Originator o("state string");
+    Memento m = o.Save();
+    EXPECT_EQ(m.State(), "state string");
+    EXPECT_EQ(m.State(), o.State());
+}
+
+TEST(MementoTest, OriginatorRestore)
+{
+    Memento m("state string");
+    Originator o;
+    o.Restore(m);
+    EXPECT_EQ(o.State(), "state string");
+    EXPECT_EQ(o.State(), m.State());
+}
+
+TEST(MementoTest, CaretakerCreate)
 {
     Caretaker c;
-    std::cout << c.State() << std::endl;
+    c.Update("1");
+    EXPECT_EQ(c.State(), "1");
+}
+
+TEST(MementoTest, CaretakerUpdate)
+{
+    Caretaker c;
+    c.Update("1");
+    c.Update("2");
+    EXPECT_EQ(c.State(), "2");
+}
+
+TEST(MementoTest, CaretakerUndo)
+{
+    Caretaker c;
+    c.Update("1");
+    c.Update("2");
+    c.Undo();
+    EXPECT_EQ(c.State(), "1");
 }
 
 }  // namespace
