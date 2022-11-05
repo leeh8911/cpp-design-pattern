18d8f2cca1dcae05a6a046de0ceb1e817a644c1e
sangwon <leeh8911@gmail.com>
2022-11-06 07:20:12 +0900

    update todo comments and repository test

--- test/behavior/observer/observer_test.cpp
+++ test/behavior/observer/observer_test.cpp
@@ -13,18 +13,36 @@
 
 // Observer pattern implementation todo list
 // TODO: 리포지토리가 업데이트 되었을 때 다른 기능들이 해당 정보를 확인할 수 있도록
-// TODO: 데이터 객체를 Data Transfer Object로 출력하는 리포지토리
 // TODO: 기능을 수행하는 객체가 리포지토리의 정보를 전달받는 것을 구현
 
 namespace
 {
 using namespace design_pattern::behavior::observer;
-TEST(ObserverTest, ImplementValueObject)
+// TODO: 특정 아이디 지정하는 생성자 추가
+TEST(ObserverTest, ObstaclesHasDifferentIds)
 {
+    // 2개의 임의의 Obstacle을 생성하여 두 개의 ID가 같은지 확인
     Obstacle obs1, obs2;
     EXPECT_NE(obs1.Id(), obs2.Id());
 
+    // 현재 생성된 Obstacle이 총 2개인지 확인
     EXPECT_EQ(Obstacle::Size(), 2);
+}
+
+// TODO: 데이터 객체를 Data Transfer Object로 출력하는 리포지토리
+// TODO: Id만을 갖는 Obstacle을 생성
+// TODO: 현재 저장된 Obstacle의 개수 출력
+// TODO: 원하는 index의 Obstacle을 출력
+// TODO: 원하는 id의 Obstacle을 출력
+// TODO: 특정 index의 Obstacle을 삭제
+//
+TEST(ObserverTest, ImplementRepository)
+{
+    ObstacleRepository repo;
+
+    repo.GenerateObstacle();
+
+    EXPECT_EQ(repo.Size(), 1);
 }
 
 } // namespace
