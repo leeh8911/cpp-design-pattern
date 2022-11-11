0311bf398da0802477275705c81d11155a504401
sangwon <leeh8911@gmail.com>
2022-11-12 05:56:30 +0900

    update comments

---
+++ test/etc/interval/interval_test.cpp
@@ -0,0 +1,24 @@
+/// @file interval_test.cpp
+/// @author sangwon (leeh8911@gmail.com)
+/// @brief
+/// @version 0.1
+/// @date 2022-11-11
+///
+/// @copyright Copyright (c) 2022
+///
+//
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+// TODO(leeh8911@gmail.com): Interval class has from and to value
+// TODO(leeh8911@gmail.com): Interval class check included value
+// TODO(leeh8911@gmail.com): Interval class check overlap other interval
+// TODO(leeh8911@gmail.com): Interval class calculate intersection other interval
+// TODO(leeh8911@gmail.com): Interval class calculate set difference other interval
+TEST(IntervalTest, CreateInterval)
+{
+}
+} // namespace
