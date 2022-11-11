02bee31e88e04fa9931a13948378a58eb8d3a50c
sangwon <leeh8911@gmail.com>
2022-11-12 06:12:41 +0900

    update test NON COMPILED

--- test/etc/interval/interval_test.cpp
+++ test/etc/interval/interval_test.cpp
@@ -45,4 +45,23 @@ TEST(IntervalTest, CheckIncludingValue)
 
     EXPECT_FALSE(reverse_inteval.IsIncluded(4.0));
 }
+
+TEST(IntervalTest, OverrlapInterval)
+{
+    Interval interval_10to12(10.0, 12.0);
+
+    Interval interval_8to9(8.0, 9.0);
+    Interval interval_8to10(8.0, 10.0);
+    Interval interval_8to11(8.0, 11.0);
+    Interval interval_11to14(11.0, 14.0);
+    Interval interval_12to14(12.0, 14.0);
+    Interval interval_13to14(13.0, 14.0);
+
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_8to9));
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_8to10));
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_8to11));
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_11to14));
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_12to14));
+    EXPECT_TRUE(interval_10to12.IsOverlap(interval_13to14));
+}
 } // namespace
