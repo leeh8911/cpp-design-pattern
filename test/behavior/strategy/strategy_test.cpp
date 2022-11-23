55ef65ec96c0201519ba5df4eb436e6697ec9a59
sangwon <leeh8911@gmail.com>
2022-11-24 07:25:11 +0900

    add test utility

--- test/behavior/strategy/strategy_test.cpp
+++ test/behavior/strategy/strategy_test.cpp
@@ -12,42 +12,63 @@
 
 #include <gtest/gtest.h>
 
+#include <stdexcept>
 #include <vector>
+
+#include "src/behavior/strategy/point.h"
 
 using namespace design_pattern::behavior::strategy;  // NOLINT
 
 namespace {
 // TODO(sangwon): 군집화 알고리즘을 다양하게 사용하기 위해 전략 패턴을
 // 적용해보자!
-// TODO(sangwon): 우선 간단하게 포인트를 정의 (포인트는 <x,y> 2차원의 위치정보를
-// 갖는 벡터, 산술연산이 필요함)
+// TODO(sangwon): 테스트를 위한 포인트 생성기
 
-// std::ostream& operator<<(std::ostream& os, const std::vector<Point>& pv) {
-//     os << "[";
-//     for (auto& p : pv) {
-//         os << p << " ";
-//     }
-//     os << "]";
-//     return os;
-// }
+std::ostream& operator<<(std::ostream& os, const std::vector<Point>& pv) {
+    os << "[";
+    for (auto& p : pv) {
+        os << p << " ";
+    }
+    os << "]";
+    return os;
+}
 
-// std::vector<Point> MakePointsOnCircle(const Point& begin, const Point& end,
-//                                       std::size_t size) {
-//     assert(size > 2);
-//     Point delta = (end - begin) / static_cast<double>(size - 1);
+std::vector<Point> MakePointsOnCircle(const Point& begin, const Point& end,
+                                      std::size_t size) {
+    if (size < 2) {
+        throw std::invalid_argument("size must be greater equal to 2");
+    }
+    Point delta = (end - begin) / static_cast<double>(size - 1);
 
-//     std::vector<Point> result{};
-//     result.reserve(size);
+    std::vector<Point> result{};
+    result.reserve(size);
 
-//     result.push_back(begin);
+    result.push_back(begin);
 
-//     for (std::size_t idx = 1; idx < size - 1; ++idx) {
-//         result.push_back(result.back() + delta);
-//     }
-//     result.push_back(end);
+    for (std::size_t idx = 1; idx < size - 1; ++idx) {
+        result.push_back(result.back() + delta);
+    }
+    result.push_back(end);
 
-//     return result;
-// }
+    return result;
+}
+
+// cppcheck-suppress syntaxError
+TEST(PointGeneratorTest, GenerateTwoPointsCase) {
+    Point begin(0.0, 0.0);
+    Point end(1.0, 1.0);
+
+    auto points = MakePointsOnCircle(begin, end, 2);
+    EXPECT_EQ(points.front(), begin);
+    EXPECT_EQ(points.back(), end);
+}
+
+TEST(PointGeneratorTest, GenerateWrongCase) {
+    Point begin(0.0, 0.0);
+    Point end(1.0, 1.0);
+
+    EXPECT_THROW(MakePointsOnCircle(begin, end, 1), std::invalid_argument);
+}
 
 // TEST(StrategyExampleTest, SampleCase) {
 //     Clusterer cluster;
