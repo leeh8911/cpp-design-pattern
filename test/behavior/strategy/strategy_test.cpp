4e7468da083f67d574109fafec57d1873568d0ad
sangwon <leeh8911@gmail.com>
2022-11-25 04:53:43 +0900

    refactory create utils

--- test/behavior/strategy/strategy_test.cpp
+++ test/behavior/strategy/strategy_test.cpp
@@ -24,52 +24,6 @@ namespace {
 // 적용해보자!
 // TODO(sangwon): 테스트를 위한 포인트 생성기
 
-std::ostream& operator<<(std::ostream& os, const std::vector<Point>& pv) {
-    os << "[";
-    for (auto& p : pv) {
-        os << p << " ";
-    }
-    os << "]";
-    return os;
-}
-
-std::vector<Point> MakePointsOnCircle(const Point& begin, const Point& end,
-                                      std::size_t size) {
-    if (size < 2) {
-        throw std::invalid_argument("size must be greater equal to 2");
-    }
-    Point delta = (end - begin) / static_cast<double>(size - 1);
-
-    std::vector<Point> result{};
-    result.reserve(size);
-
-    result.push_back(begin);
-
-    for (std::size_t idx = 1; idx < size - 1; ++idx) {
-        result.push_back(result.back() + delta);
-    }
-    result.push_back(end);
-
-    return result;
-}
-
-// cppcheck-suppress syntaxError
-TEST(PointGeneratorTest, GenerateTwoPointsCase) {
-    Point begin(0.0, 0.0);
-    Point end(1.0, 1.0);
-
-    auto points = MakePointsOnCircle(begin, end, 2);
-    EXPECT_EQ(points.front(), begin);
-    EXPECT_EQ(points.back(), end);
-}
-
-TEST(PointGeneratorTest, GenerateWrongCase) {
-    Point begin(0.0, 0.0);
-    Point end(1.0, 1.0);
-
-    EXPECT_THROW(MakePointsOnCircle(begin, end, 1), std::invalid_argument);
-}
-
 // TEST(StrategyExampleTest, SampleCase) {
 //     Clusterer cluster;
 
---
+++ test/utils/point_util_test.cpp
@@ -0,0 +1,51 @@
+/// @file point_util_test.cpp
+/// @author sangwon (leeh8911@gmail.com)
+/// @brief
+/// @version 0.1
+/// @date 2022-11-24
+///
+/// @copyright Copyright (c) 2022
+///
+//
+
+#include "test/utils/point_util.h"
+
+#include <gtest/gtest.h>
+
+#include "src/behavior/strategy/point.h"
+
+using namespace design_pattern::behavior::strategy::test;  // NOLINT
+using namespace design_pattern::behavior::strategy;        // NOLINT
+
+namespace {
+// cppcheck-suppress syntaxError
+TEST(PointGeneratorTest, GenerateTwoPointsCase) {
+    Point begin(0.0, 0.0);
+    Point end(1.0, 1.0);
+
+    auto points = MakePointsOnCircle(begin, end, 2);
+    EXPECT_EQ(points.front(), begin);
+    EXPECT_EQ(points.back(), end);
+}
+
+// cppcheck-suppress syntaxError
+TEST(PointGeneratorTest, GenerateWrongCase) {
+    Point begin(0.0, 0.0);
+    Point end(1.0, 1.0);
+
+    EXPECT_THROW(MakePointsOnCircle(begin, end, 1), std::invalid_argument);
+}
+
+TEST(PointGeneratorTest, GenerateTenPointsCase) {
+    Point begin(0.0, 0.0);
+    Point end(1.0, 1.0);
+
+    auto points = MakePointsOnCircle(begin, end, 10);
+
+    EXPECT_EQ(10, points.size());
+    EXPECT_EQ(begin, points.front());
+    EXPECT_EQ(end, points.back());
+
+    EXPECT_EQ(Point(0.1, 0.1), points.at(1));
+}
+}  // namespace
