2d2f2a5f2d3aaa06f47ec07d701e1e81729ecd14
sangwon <leeh8911@gmail.com>
2022-11-26 07:33:40 +0900

    refactory test case

--- test/behavior/strategy/strategy_test.cpp
+++ test/behavior/strategy/strategy_test.cpp
@@ -30,36 +30,25 @@ namespace {
 // TODO(sangwon): 군집화 알고리즘을 다양하게 사용하기 위해 전략 패턴을
 // 적용해보자!
 
-TEST(StrategyExampleTest, NoClusteringCase) {
-    Cluster cluster(std::make_unique<BasicCluster>(0.0));
-    std::vector<Point> X = MakePointsOnLineSegment(Point(1.0, -1.0), Point(1.0, 1.0), 10);
+class StrategyExampleTest : public testing::Test {
+ public:
+    void SetUp() override {
+        std::vector<Point> X_1 = MakePointsOnLineSegment(Point(1.0, -1.0), Point(1.0, 1.0), 10);
+        std::vector<Point> X_2 = MakePointsOnLineSegment(Point(1.0, 2.0), Point(1.0, 3.0), 10);
 
-    bool success = cluster.Fit(X);
+        std::vector<Point> X{};
+        X.reserve(X_1.size() + X_2.size());
 
-    EXPECT_TRUE(success);
-    EXPECT_EQ(X.size(), cluster.LabelSize()) << cluster;
-}
+        std::copy(X_1.begin(), X_1.end(), std::back_inserter(X));
+        std::copy(X_2.begin(), X_2.end(), std::back_inserter(X));
+    }
+    void TearDown() override {}
 
-TEST(StrategyExampleTest, OneLabelCase) {
+    std::vector<Point> X;
+};
+
+TEST_F(StrategyExampleTest, TwoLabelCase) {
     Cluster cluster(std::make_unique<BasicCluster>(1.0));
-    std::vector<Point> X = MakePointsOnLineSegment(Point(1.0, -1.0), Point(1.0, 1.0), 10);
-
-    bool success = cluster.Fit(X);
-
-    EXPECT_TRUE(success);
-    EXPECT_EQ(1, cluster.LabelSize());
-}
-
-TEST(StrategyExampleTest, TwoLabelCase) {
-    Cluster cluster(std::make_unique<BasicCluster>(1.0));
-    std::vector<Point> X_1 = MakePointsOnLineSegment(Point(1.0, -1.0), Point(1.0, 1.0), 10);
-    std::vector<Point> X_2 = MakePointsOnLineSegment(Point(1.0, 2.0), Point(1.0, 3.0), 10);
-
-    std::vector<Point> X{};
-    X.reserve(X_1.size() + X_2.size());
-
-    std::copy(X_1.begin(), X_1.end(), std::back_inserter(X));
-    std::copy(X_2.begin(), X_2.end(), std::back_inserter(X));
 
     bool success = cluster.Fit(X);
 
@@ -67,17 +56,8 @@ TEST(StrategyExampleTest, TwoLabelCase) {
     EXPECT_EQ(2, cluster.LabelSize());
 }
 
-TEST(StrategyExampleTest, DBSCANTwoLabelCase) {
+TEST_F(StrategyExampleTest, DBSCANTwoLabelCase) {
     Cluster cluster(std::make_unique<DBSCAN>(1.0, 5));
-
-    std::vector<Point> X_1 = MakePointsOnLineSegment(Point(1.0, -1.0), Point(1.0, 1.0), 10);
-    std::vector<Point> X_2 = MakePointsOnLineSegment(Point(1.0, 2.0), Point(1.0, 3.0), 10);
-
-    std::vector<Point> X{};
-    X.reserve(X_1.size() + X_2.size());
-
-    std::copy(X_1.begin(), X_1.end(), std::back_inserter(X));
-    std::copy(X_2.begin(), X_2.end(), std::back_inserter(X));
 
     bool success = cluster.Fit(X);
 
